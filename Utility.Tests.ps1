c19b9270aedf8fc6f313153bfc983e22926d1796
Administrator <customer.care@ctl.io>
2019-03-30 16:58:14 +0000

    no message

---
+++ Utility.Tests.ps1
@@ -0,0 +1,28 @@
+﻿InModuleScope Utility {
+    Describe 'Utility Module Test' {
+        It 'Invoke-Parallel Test 1' {
+            $var = [System.Collections.Queue]::new()
+            Invoke-Parallel -ParameterSet @{}, @{} -ScriptBlock {
+                $SyncronizedObject.Enqueue($pid)
+                $SyncronizedObject.Count
+            } -Method RunspacePool -SyncronizedObject $var
+            $var.Count | Should Be 2
+        }
+
+        It 'Invoke-Parallel Test 2' {
+            $script = {
+                Invoke-Parallel -ParameterSet @(
+                    @{target = 'google.com'}
+                ) -ScriptBlock {
+                    param ($target)
+                    [net.dns]::GetHostByName($target).AddressList[0]
+                }
+            }
+            $script | Should Not throw
+        }
+    }
+}
+
+<#
+Invoke-Pester -Script $HOME\Documents\AppVeyorTest\Utility.Tests.ps1
+#>
